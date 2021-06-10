834b2a94e658587de34735caa3a436da9ab98048
nkusumo <nis.kusumo@gmail.com>
2021-06-10 13:38:14 -0700

    skeleton

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,12 @@
 import video from "../data/video.js";
+import Video from "./Video"
 
 function App() {
   console.log("Here's your data:", video);
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        frameborder="0"
-        allowfullscreen
-        title="Thinking in React"
-      />
+      <Video videoObj={video} />
     </div>
   );
 }
